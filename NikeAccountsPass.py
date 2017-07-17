3ce6ae783f85625a371494f8570377018e3f22be
MySneakerPlug <mysplug@gmail.com>
2017-07-17 05:30:23 -0400

    Update NikeAccountsPass.py

--- NikeAccountsPass.py
+++ NikeAccountsPass.py
@@ -1,6 +1,4 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> #Script to change password in a list of nike+ accounts
+#Script to change password in a list of nike+ accounts
 #Works for Nike US and Nike EU
 #The account file should contain email:password in each line for each account
 
